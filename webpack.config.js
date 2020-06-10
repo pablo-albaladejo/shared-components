3549608ec9746a05d38c68af0179a0c540321fab
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-06-10 15:44:11 +0200

    chore(master): updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
+var path = require('path');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
-  };
+      }
+    ],
+  }
+};
